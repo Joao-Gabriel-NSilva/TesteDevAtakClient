a195d5ffcd68fb730389c14300c9df9caebfd203
João Gabriel Nascimento da Silva <75106078+Joao-Gabriel-NSilva@users.noreply.github.com>
2022-11-27 17:19:01 -0300

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function ($) {
+    $("#search-input").on('keypress', function (e) {
+        if (e.which == 13) {
+            exibeResultado();
+        }
+    });
+
+    $("section.search-container > div > div > i").click(function() {
+        exibeResultado();
+    })
+});
+
+async function busca() {
+    let inputValue = $("#search-input").val().trim();
+    if (inputValue != "") {
+        let dadosFetch = await
+            fetch(`https://apitestedevatak20221126200451.azurewebsites.net/api/Busca?busca=${inputValue}`);
+
+        let json = dadosFetch.json();
+        return json;
+    }
+}
+
+function exibeResultado() {
+    let json = busca();
+    $("li").each(function () {
+        $(this).remove();
+    });
+
+    json.then((result) => {
+        for (const resultado of result.Resultados) {
+            $(".lista-de-resultados").append(`<li><div><h2>${resultado.Titulo}</h2><a href=\"${resultado.Link}\" target=\"_blank\">${resultado.Link}</a></div></li>`);
+        }
+    })
+    $("#search-input").val("");
+    $("#search-input").blur()
+}
