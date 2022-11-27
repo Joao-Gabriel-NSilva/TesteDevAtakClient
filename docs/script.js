2852ba983d17fdfb31c3f360946fb49f5d9528a3
João Gabriel Nascimento da Silva <75106078+Joao-Gabriel-NSilva@users.noreply.github.com>
2022-11-27 17:51:05 -0300

    alteração no script

--- docs/script.js
+++ docs/script.js
@@ -5,33 +5,33 @@ $(document).ready(function ($) {
         }
     });
 
-    $("section.search-container > div > div > i").click(function() {
+    $("section.search-container > div > div > i").click(function () {
         exibeResultado();
     })
 });
 
-async function busca() {
-    let inputValue = $("#search-input").val().trim();
-    if (inputValue != "") {
-        let dadosFetch = await
-            fetch(`https://apitestedevatak20221126200451.azurewebsites.net/api/Busca?busca=${inputValue}`);
+async function busca(inputValue) {
+    let dadosFetch = await
+        fetch(`https://apitestedevatak20221126200451.azurewebsites.net/api/Busca?busca=${inputValue}`);
+    let json = dadosFetch.json();
+    return json;
 
-        let json = dadosFetch.json();
-        return json;
-    }
 }
 
 function exibeResultado() {
-    let json = busca();
-    $("li").each(function () {
-        $(this).remove();
-    });
+    let inputValue = $("#search-input").val().trim();
+    if (inputValue != "") {
+        let json = busca(inputValue);
+        $("li").each(function () {
+            $(this).remove();
+        });
 
-    json.then((result) => {
-        for (const resultado of result.Resultados) {
-            $(".lista-de-resultados").append(`<li><div><h2>${resultado.Titulo}</h2><a href=\"${resultado.Link}\" target=\"_blank\">${resultado.Link}</a></div></li>`);
-        }
-    })
-    $("#search-input").val("");
-    $("#search-input").blur()
+        json.then((result) => {
+            for (const resultado of result.Resultados) {
+                $(".lista-de-resultados").append(`<li><div><h2>${resultado.Titulo}</h2><a href=\"${resultado.Link}\" target=\"_blank\">${resultado.Link}</a></div></li>`);
+            }
+        })
+        $("#search-input").val("");
+        $("#search-input").blur()
+    }
 }
